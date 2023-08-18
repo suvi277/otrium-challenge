1e858190bafaed9be5eb9b559c72eb027215bebc
suvarna.mondal <suvarna.mondal@johnlewis.co.uk>
2023-08-18 11:19:12 +0100

    Updates

--- src/types.ts
+++
@@ -1,9 +0,0 @@
-export type CategoryType = {
-    id: string;
-    count: number;
-    parent: string;
-    name: string;
-    children?: CategoryType[];
-};
-
-export type CategoryIdName = { id: string; name: string };
